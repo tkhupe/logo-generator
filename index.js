9ab1fde19d07c14e351ad4ebbbfaf70e75dfd723
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-04-17 10:50:18 -0500

    added prompts for logo generation

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const {writeToFile, Svg} = require('./lib/utils');
 const inquirer = require('inquirer');
 const {Circle, Square, Triangle} = require('./lib/utils');
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Enter up to the characters you want to display",
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Enter the color, or the color code of the text you want to display",
+    },
+    {
+        type: "list",
+        name: "shapes",
+        message: "Choose the shape of your logo",
+        choices: ["Circle", "Square", "Triangle"],
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter the color, or the color code of the shape you want",
+    },
+];
